8d4da302a126c6c087f1496c4153d321f2a25fd2
GET_SIZE <zenyagaydey@gmail.com>
2025-11-12 12:35:56 +0200

    init project and create models

---
+++ main/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(**args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 related_name='products')
+    color = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    main_image = models.ImageField(upload_to='products/main/')
+    stock = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(**args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def is_in_stock(self):
+        return self.stock > 0
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                related_name='images')
+    image = models.ImageField(upload_to='products/extra/')
