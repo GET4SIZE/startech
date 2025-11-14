6a4dc443c3540c2181845849df66997e5fbb2211
GET_SIZE <zenyagaydey@gmail.com>
2025-11-14 23:06:41 +0200

    made views /urls/admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage
+
 
 # Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'color', 'price', 'stock']
+    list_filter = ['category', 'color']
+    search_fields = ['name', 'color', 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
